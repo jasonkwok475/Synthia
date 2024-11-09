3e0a8958133fd24b8f4391d0b51babbf5f37022c
Jason <jasonkwok375@gmail.com>
2024-11-08 18:39:42 -0800

    Added basic sections

--- main_window/main_app.py
+++ main_window/main_app.py
@@ -16,10 +16,34 @@ class MainWindow(QWidget):
     self.showMaximized()
 
   def initUi(self):
+    logTextBox = QTextEditLogger(self)
+    logTextBox.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s --- %(message)s'))
+    logging.getLogger().addHandler(logTextBox)
+
     self.grid = QGridLayout()
+    self.grid.setRowStretch(1,2) 
+    self.grid.setRowStretch(2,2) 
+    self.grid.setColumnStretch(1,2) 
+    self.grid.setColumnStretch(2,1) 
+    self.grid.setColumnStretch(3,1) 
+
+    #Program Logs
+    self.loggingGroupBox = QGroupBox("Network Logs")
+    self.vboxLog = QVBoxLayout()
+    self.vboxLog.addWidget(logTextBox.widget)
+    self.loggingGroupBox.setLayout(self.vboxLog)
+
+    #Add widgets to display
+    self.grid.addWidget(QGroupBox("3D Plot Here"), 1, 1, 2, 1)
+    self.grid.addWidget(QGroupBox("2D Plot Here"), 1, 2, 2, 1)
+    self.grid.addWidget(QGroupBox("Buttons Here"), 1, 3)
+    self.grid.addWidget(QGroupBox("Buttons Here"), 2, 3)
+    self.grid.addWidget(self.loggingGroupBox, 3, 1, 1, 3)
 
     self.setLayout(self.grid)
 
- 
-
-  
+class QTextEditLogger(logging.Handler):
+  def __init__(self, parent):
+      super().__init__()
+      self.widget = QPlainTextEdit(parent)
+      self.widget.setReadOnly(True) 
